af45d6ae4b7ae589149e8376b4065df9c85bcd4f
BeyondeLimit <votemap62@gmail.com>
2023-01-07 22:51:32 +0100

    Add weather forecast reports

--- src/services/weather-finding-service.ts
+++ src/services/weather-finding-service.ts
@@ -1,4 +1,5 @@
-import { APIOneCallWeather, IWeatherResponse } from "../api/api-one-call";
+import { IWeatherResponse } from "../../models/i-weather-response";
+import { APIOneCallWeather } from "../api/api-one-call";
 import { Location } from "telegraf/typings/core/types/typegram";
 
 
@@ -13,7 +14,18 @@ export class WeatherFindingService {
         const temperature = weather.current_weather.temperature;
         const dailyUnits = weather.daily_units.temperature_2m_max;
         const time = weather.current_weather.time.split('T')[1];
+        
+        const formatter = new Intl.DateTimeFormat('en', { month: 'short' });
+        const forecast = weather.daily.time.map((item, index) => {
+            const date = new Date(item).toDateString();
+            return `\nDate: ${date}, min: ${weather.daily.temperature_2m_min[index]}${dailyUnits} max: ${weather.daily.temperature_2m_max[index]}${dailyUnits}`;
+        });
 
-        return `Currently your weather is: ${temperature} ${dailyUnits} \nAt ${time} o'clock`;
+        const initialMessage = `
+        Currently your weather is: ${temperature} ${dailyUnits} \nAt ${time} o'clock.
+
+Weather forecast for upcomming ${weather.daily.time.length} days is:
+        `;
+        return initialMessage + forecast;
     };
 }
